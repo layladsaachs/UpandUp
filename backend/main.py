362aac02e36e7a147b0d18921aeacb2ebb98ef44
jovannyreyes-23 <jovannyreyes.23@csu.fullerton.edu>
2026-02-18 14:00:53 -0800

    Added auth route to prepare for Spotify login and authentication flow

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from routes.auth import router as auth_router
 
 app = FastAPI()
 @app.get("/")
 def root():
     return {"status": "Backend Running"}
+
+app.include_router(auth_router)
